92437737cbf01dddd8c6503a2823ca589727dbde
André Bauer <monotek23@gmail.com>
2020-11-03 13:22:11 +0100

    added github actions
    Signed-off-by: André Bauer <monotek23@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM ubuntu:focal
+
+ARG DEBIAN_FRONTEND=noninteractive
+
+ARG LAUNCHPAD_UPLOAD="no"
+ARG OS_VERSIONS="groovy"
+ARG PACKAGE_VERSION="5.3.7"
+
+WORKDIR /tmp
+
+COPY fwbuilder.sh config.dist /tmp
+
+RUN apt-get update -y;apt-get -y upgrade; \
+    apt-get install -y build-essential cdbs cmake devscripts dput libsnmp-dev libxml2-dev libxslt1-dev qtbase5-dev qttools5-dev-tools zlib1g-dev 
+
+RUN sed -r -e "s#DEBEMAIL=.*#DEBEMAIL=\"${DEBEMAIL}\"#g" \
+    -e "s#DEBFULLNAME=.*#DEBFULLNAME=\"${DEBFULLNAME}\"#g" \
+    -e "s#LAUNCHPAD_UPLOAD=.*#LAUNCHPAD_UPLOAD=\"${LAUNCHPAD_UPLOAD}\"#g" \
+    -e "s#OS_VERSIONS=.*#OS_VERSIONS=\"${OS_VERSIONS}\"#g" \
+    -e "s#PACKAGE_VERSION=.*#PACKAGE_VERSION=\"${PACKAGE_VERSION}\"#g" \
+    -e "s#PPA_OWNER=.*#PPA_OWNER=\"${PPA_OWNER}\"#g" < /tmp/config.dist > /tmp/config
+
+RUN ./fwbuilder.sh
--- fwbuilder.sh
+++ fwbuilder.sh
@@ -3,6 +3,8 @@
 # build fwbuilder
 #
 
+set -e
+
 if [ ! -f config ]; then
     echo "need config file. create from config.dist before running this script!"
     exit 1
@@ -12,35 +14,34 @@ fi
 . "config"
 
 #script
-if [ -z ${OS_VERSION} ]; then
-    echo -e "need os & package version! \nUsage: $0 trusty 5.3.7"
-    exit 1
-fi
+for OS_VERSION in ${OS_VERSIONS}; do
+    echo -e "\n#######################\nbuilding ${PACKAGE} ${PACKAGE_VERSION} for OS ${OS_VERSION}\n#######################\n"
 
-export DEBFULLNAME=${DEBFULLNAME}
+    export DEBFULLNAME=${DEBFULLNAME}
 
-export DEBEMAIL=${DEBEMAIL}
+    export DEBEMAIL=${DEBEMAIL}
 
-if [ "${2}" == "nodelete" ]; then
-    echo "no delete! uploading new version!"
-    cd ${BUILD_DIR}
-else
-    test -d ${BUILD_DIR} && rm -rf ${BUILD_DIR}
-    test -d ${BUILD_DIR} || mkdir -p ${BUILD_DIR}
-    cd ${BUILD_DIR}
-    git clone ${FWBUILDER_GIT_REPO}
-fi
+    if [ "${2}" == "nodelete" ]; then
+        echo "no delete! uploading new version!"
+        cd ${BUILD_DIR}
+    else
+        test -d ${BUILD_DIR} && rm -rf ${BUILD_DIR}
+        test -d ${BUILD_DIR} || mkdir -p ${BUILD_DIR}
+        cd ${BUILD_DIR}
+        git clone ${FWBUILDER_GIT_REPO}
+    fi
 
-REAL_PATH="$(realpath .)"
+    REAL_PATH="$(realpath .)"
 
-cd $(find ${REAL_PATH} -maxdepth 1 -mindepth 1 -type d -name "*${PACKAGE}*")/debian
+    cd $(find ${REAL_PATH} -maxdepth 1 -mindepth 1 -type d -name "*${PACKAGE}*")/debian
 
-debchange -D ${OS_VERSION} -v ${PACKAGE_VERSION}-${OS_VERSION}1 ${DEBCOMMENT}
+    debchange -D ${OS_VERSION} -v ${PACKAGE_VERSION}-${OS_VERSION}1 ${DEBCOMMENT}
 
-if [ "${LAUNCHPAD_UPLOAD}" == "yes" ]; then
-    debuild -S
+    if [ "${LAUNCHPAD_UPLOAD}" == "yes" ]; then
+        debuild -S
 
-    dput ppa:${PPA_OWNER}/${PPA} $(find ${REAL_PATH} -name ${PACKAGE}*_source.changes | sort | tail -n1)
-else
-    debuild -us -uc -i -I
-fi
+        dput ppa:${PPA_OWNER}/${PPA} $(find ${REAL_PATH} -name ${PACKAGE}*_source.changes | sort | tail -n1)
+    else
+        debuild -us -uc -i -I
+    fi
+done
